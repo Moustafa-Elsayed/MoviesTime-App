6619ad8ca51a2c1336b75356e46537e8c0422ffd
moustafa elsayed <moelsayed949@gmail.com>
2024-01-27 20:10:52 +0200

    init MoviesTime APP

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-native', 'jsx-a11y', 'import', 'react-hooks'],
+  rules: {
+    // Add your specific ESLint rules here
+    // Example:
+    // 'semi': ['error', 'always'],
+    // 'quotes': ['error', 'single'],
+    // ...
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
 };
