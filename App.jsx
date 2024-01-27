8db39bba99ed954dc6091e50392bed06f3a5e823
moustafa elsayed <moelsayed949@gmail.com>
2024-01-27 20:18:42 +0200

    init App

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: 'red',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
